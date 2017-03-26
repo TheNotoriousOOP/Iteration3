f441c8b0e7f2618c1552e48be7f37c017da3850e
Or Oz <oroz77@gmail.com>
2017-03-26 13:37:36 -0400

    basic implementation details

--- src/main/model/map/tile/PastureTile.java
+++ src/main/model/map/tile/PastureTile.java
@@ -1,9 +1,25 @@
 package model.map.tile;
 
+import model.map.CubeVector;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
- * Class Description:
+ * Class Description: a subclass of LandTile
  * Responsibilities:
+ *  producing livestock
+ *  irrigating a desert tile by replacing it with a pasture tile
  */
-public class PastureTile {
+public class PastureTile extends LandTile {
+    PastureTile(CubeVector location, Zone[] zones) {
+        super(location, zones);
+    }
+
+    /*
+    //copy constructor for irrigation. only needed for phase 2
+    public void PastureTile(DesertTile desertToPasture){
+        setLocation(desertToPasture.getLocation());
+        setZones(desertToPasture.getZones());
+    }
+    */
+
 }
