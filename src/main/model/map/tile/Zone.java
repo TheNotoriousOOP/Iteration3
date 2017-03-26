18be1a80bba8328434a8993e8f2c534bf6e9f206
Or Oz <oroz77@gmail.com>
2017-03-26 13:11:12 -0400

    added constructor and local variables

--- src/main/model/map/tile/Zone.java
+++ src/main/model/map/tile/Zone.java
@@ -2,8 +2,28 @@ package model.map.tile;
 
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
- * Class Description:
+ * Class Description: represents the division of a hex tile
  * Responsibilities:
+ *  for editor deliverable: only used for water matching validation
  */
 public class Zone {
+    private boolean hasWater;
+    private boolean isMerged;
+
+    public Zone(boolean hasWater, boolean isMerged){
+        this.hasWater = hasWater;
+        this.isMerged = isMerged;
+    }
+
+    public boolean isHasWater() {
+        return hasWater;
+    }
+
+    public boolean isMerged() {
+        return isMerged;
+    }
+
+    public void setMerged(boolean merged) {
+        isMerged = merged;
+    }
 }
