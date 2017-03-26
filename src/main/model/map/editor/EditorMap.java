2a528ec01a3925d0dcd59f7144a4e313502ec1c4
Andrew <aknoxlor@gmail.com>
2017-03-26 14:10:11 -0400

    Changed EditorMap to hashmap

--- src/main/model/map/editor/EditorMap.java
+++ src/main/model/map/editor/EditorMap.java
@@ -5,47 +5,49 @@ import model.map.MapInterface;
 import model.map.tile.Tile;
 import model.map.tile.Zone;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description: Defines the game map as a grid of Tiles
  * Responsibilities: Storing, Accessing, and Mutating map grid of Tiles
+ * Implementation: Stored as Hashmap
  */
 public class EditorMap implements MapInterface {
 
     private int maxRadius;
-    private Tile[][] map;
+    private Map<CubeVector, Tile> map;
 
     public EditorMap(int maxRadius) {
         if(maxRadius < 1) {
             System.out.printf("ERROR: Invalid maxRadius in EditorMap\n");
         }
-
         this.maxRadius = maxRadius;
-        this.map = new Tile[maxRadius*2+1][maxRadius*2+1];
+        //int size = (maxRadius+1) * (maxRadius+1) * (maxRadius+1) + 1;
+        this.map = new HashMap<CubeVector, Tile>();
     }
 
     @Override
     public Tile getTile(CubeVector pos) {
-        return null;
+        if(!coordinateCheck(pos))
+            System.out.printf("Invalid Coordinate");
+        return map.get(pos);
     }
 
     @Override
     public Tile getNeighborTile(CubeVector pos, Zone bordering) {
+        //May not need this anymore
         return null;
     }
 
     @Override
-    public void load(String[] dataa) {
+    public void load(String[] data) {
 
     }
 
-    //Implementation detail
-    private int cubeToOffX(CubeVector v) {
-        return 0;
-    }
-
-    //Implementation detail
-    private int cubeToOffY(CubeVector v) {
-        return 0;
+    private boolean coordinateCheck(CubeVector pos) {
+        int dis = Math.abs(pos.getXCoord()) + Math.abs(pos.getZCoord()) + Math.abs(pos.getYCoord()) / 2;
+        return dis <= maxRadius;
     }
 }
