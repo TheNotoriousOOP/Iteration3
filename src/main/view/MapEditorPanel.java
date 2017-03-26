52b6e40209b4276fffc37c3579a54c38acc1a7e8
thkim1234 <tedkim1234@gmail.com>
2017-03-26 17:17:25 -0400

    Main Menu Done

--- src/main/view/MapEditorPanel.java
+++ src/main/view/MapEditorPanel.java
@@ -1,9 +1,24 @@
 package view;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description:
  * Responsibilities:
  */
-public class MapEditorPanel {
+public class MapEditorPanel extends Panel{
+    private JTextField mapName;
+    private JButton exit;
+    private JButton save;
+    private JButton addOrRemove;
+
+    public MapEditorPanel(){
+
+        this.setLayout(new GridBagLayout());
+        this.exit = new JButton("Exit");
+        this.save = new JButton("Save");
+        this.mapName = new JTextField("Map Name");
+    }
 }
