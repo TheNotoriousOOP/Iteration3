599933178107a05c4e2b7ee57940519c9c047b23
Andrew <aknoxlor@gmail.com>
2017-03-26 13:45:17 -0400

    Start implementation of EditorMap

--- src/main/model/map/editor/EditorMap.java
+++ src/main/model/map/editor/EditorMap.java
@@ -7,12 +7,22 @@ import model.map.tile.Zone;
 
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
- * Class Description:
- * Responsibilities:
+ * Class Description: Defines the game map as a grid of Tiles
+ * Responsibilities: Storing, Accessing, and Mutating map grid of Tiles
  */
 public class EditorMap implements MapInterface {
 
-    
+    private int maxRadius;
+    private Tile[][] map;
+
+    public EditorMap(int maxRadius) {
+        if(maxRadius < 1) {
+            System.out.printf("ERROR: Invalid maxRadius in EditorMap\n");
+        }
+
+        this.maxRadius = maxRadius;
+        this.map = new Tile[maxRadius*2+1][maxRadius*2+1];
+    }
 
     @Override
     public Tile getTile(CubeVector pos) {
@@ -27,5 +37,15 @@ public class EditorMap implements MapInterface {
     @Override
     public void load(String[] dataa) {
 
+    }
+
+    //Implementation detail
+    private int cubeToOffX(CubeVector v) {
+        return 0;
+    }
+
+    //Implementation detail
+    private int cubeToOffY(CubeVector v) {
+        return 0;
     }
 }
