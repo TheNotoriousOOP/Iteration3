ff1324acad5cba8f61d898749b151b4a1a3c9d8d
Or Oz <oroz77@gmail.com>
2017-03-26 13:26:49 -0400

    extends Tile, added super constructor

--- src/main/model/map/tile/NonLandTile.java
+++ src/main/model/map/tile/NonLandTile.java
@@ -1,10 +1,15 @@
 package model.map.tile;
 
+import model.map.CubeVector;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description: a subtype of tile, used for delineating between Land and NonLand
  * Responsibilities: provide standard functionality for all Tiles with NonLand Terrains
  */
-public abstract class NonLandTile {
+public abstract class NonLandTile extends Tile {
 
+    NonLandTile(CubeVector location, Zone[] zones) {
+        super(location, zones);
+    }
 }
