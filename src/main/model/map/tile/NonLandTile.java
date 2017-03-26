91375db65026786d1ec8aeb884707ac396010a2a
Or Oz <oroz77@gmail.com>
2017-03-26 13:14:56 -0400

    added comments

--- src/main/model/map/tile/NonLandTile.java
+++ src/main/model/map/tile/NonLandTile.java
@@ -2,8 +2,9 @@ package model.map.tile;
 
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
- * Class Description:
- * Responsibilities:
+ * Class Description: a subtype of tile, used for delineating between Land and NonLand
+ * Responsibilities: provide standard functionality for all Tiles with NonLand Terrains
  */
-public class NonLandTile {
+public abstract class NonLandTile {
+    
 }
