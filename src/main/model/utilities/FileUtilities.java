c9f25b0837abc992410d5ed4393caa6d58548a1c
David Wetzel <LaxMiddie@live.com>
2017-03-26 14:04:27 -0400

    Added Utilities description

--- src/main/model/utilities/FileUtilities.java
+++ src/main/model/utilities/FileUtilities.java
@@ -3,7 +3,22 @@ package model.utilities;
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description:
+ *      Creates and writes to or loads a .txt file filled with
  * Responsibilities:
+ *      Creates a file of the map a user wants to save
+ *      Gives the map the tiles that it will be composed of as a String Array
  */
 public class FileUtilities {
+    private String[] array;
+
+    public FileUtilities(){
+    }
+
+    public void saveMap(String fileName, String[] array){
+        this.array = array;
+    }
+
+    public String[] loadMap(String filename){
+        return array;
+    }
 }
--- src/main/model/utilities/TileUtilities.java
+++ src/main/model/utilities/TileUtilities.java
@@ -1,9 +1,32 @@
 package model.utilities;
 
+import model.map.CubeVector;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description:
+ *      Determines whether tile places on EditorMap are valid or not
  * Responsibilities:
+ *      Checks whether TileToBePlaced is being placed adjacent to a tile already on the map
+ *      Ensures rivers of two tiles are connected
+ *      Computes the center of gravity
+ *      Ensures distance between 2 tiles is no larger than 21
  */
 public class TileUtilities {
+    public TileUtilities(){
+
+    }
+    public boolean calculateDistance(){
+        return true;
+    }
+
+    public boolean doWaterSourcesMatch(){
+        return true;
+    }
+    public CubeVector calculateCenterOfGravity(){
+        return new CubeVector(0,0,0);
+    }
+    public boolean checkAdjcency(){
+        return true;
+    }
 }
