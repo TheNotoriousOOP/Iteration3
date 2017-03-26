978e27115b0fb6d637a8756577e96d7aa0c81be8
TylerBarkley <tylerb306@ufl.edu>
2017-03-26 13:18:46 -0400

    Fixed tile

--- src/main/model/map/tile/Tile.java
+++ src/main/model/map/tile/Tile.java
@@ -1,9 +1,23 @@
 package model.map.tile;
 
+import model.map.CubeVector;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description:
  * Responsibilities:
  */
-public class Tile {
+public abstract class Tile {
+
+    private CubeVector location;
+    private Zone[] borderZone;
+
+    Tile(CubeVector location, Zone[] borderZone){
+        this.location = location;
+        this.borderZone = borderZone;
+    }
+
+    //public abstract void buildStructure(Structure target);
+    //Not needed until Phase 2
+
 }
