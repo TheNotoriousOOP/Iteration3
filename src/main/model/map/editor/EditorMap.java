b961c74b17671ef024d86dcf242aaa8fe8cd660a
Andrew <aknoxlor@gmail.com>
2017-03-26 13:22:23 -0400

    Defined MapInterface

--- src/main/model/map/editor/EditorMap.java
+++ src/main/model/map/editor/EditorMap.java
@@ -1,9 +1,31 @@
 package model.map.editor;
 
+import model.map.CubeVector;
+import model.map.MapInterface;
+import model.map.tile.Tile;
+import model.map.tile.Zone;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description:
  * Responsibilities:
  */
-public class EditorMap {
+public class EditorMap implements MapInterface {
+
+    
+
+    @Override
+    public Tile getTile(CubeVector pos) {
+        return null;
+    }
+
+    @Override
+    public Tile getNeighborTile(CubeVector pos, Zone bordering) {
+        return null;
+    }
+
+    @Override
+    public void load(String[] dataa) {
+
+    }
 }
