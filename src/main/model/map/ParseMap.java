e7e705c9f3f2d6b390d87a9fc272372fcb32710d
Or Oz <oroz77@gmail.com>
2017-03-27 17:40:26 -0400

    added new parsing logic

--- src/main/model/map/ParseMap.java
+++ src/main/model/map/ParseMap.java
@@ -29,10 +29,86 @@ public class ParseMap {
 
             @Override
             public Object next() {
-                return parse(tileData[itr++]);
+                return parseStringForTile(tileData[itr++]);
             }
         };
     }
+
+    //sample '( 1 0 0 ) sea ( 1 4 6 )'
+    private Tile parseStringForTile(String s) {
+
+        s = s.replaceAll("\\s+","");    //remove all whitespaces
+
+        //sample (100)sea(146)
+
+        boolean hasRiver = false;
+        int numberOfParenthesesPair = s.length() - s.replaceAll("\\)", "").length();  //count pairs of parentheses
+
+        if (numberOfParenthesesPair > 1 ){  //number of pair parentheses = 1 if nonrivered
+            hasRiver = true;
+        }
+
+        int indexOfLocationLeftParenthesis = s.indexOf('(');    //0
+        int indexOfLocationRightParenthesis = s.indexOf(')', indexOfLocationLeftParenthesis) + indexOfLocationLeftParenthesis;  //4
+
+        String locationString = s.substring(indexOfLocationLeftParenthesis + 1, indexOfLocationRightParenthesis);   //0-4 TODO check
+
+
+        int indexOfTerrainBegin = indexOfLocationRightParenthesis + 1; //5
+        int indexOfTerrainEnd = s.length(); //~12
+        if(hasRiver){
+            indexOfTerrainEnd = s.lastIndexOf('(') - 1; //7
+        }
+
+        String terrain = s.substring(indexOfTerrainBegin, indexOfTerrainEnd + 1); //5-7 TODO check
+
+        Zone[] zones = new Zone[6];
+        //TODO check if blank zones are made by default
+
+        if (hasRiver){
+            int indexOfRiverStart = indexOfTerrainEnd + 1; //8
+            int indexOfRiverEnd = s.length(); //10
+
+            String river = s.substring(indexOfRiverStart + 1, indexOfRiverEnd);
+
+
+
+            for (Character c : river.toCharArray()){
+                int zoneFace = Character.getNumericValue(c);
+                zones[zoneFace-1] = new Zone(true, false);
+            }
+
+        }
+
+        int x = Character.getNumericValue(locationString.charAt(0));
+        int y = Character.getNumericValue(locationString.charAt(1));
+        int z = Character.getNumericValue(locationString.charAt(2));
+
+        CubeVector location = new CubeVector(x, y, z);
+
+        System.out.println("Terrain: "  + terrain);
+        System.out.println("Coord: " + locationString);
+        System.out.println("rivers? : " + hasRiver  + "\n");
+
+        switch (terrain){
+            case "woods":
+                return new WoodsTile(location, zones);
+            case "pasture":
+                return new PastureTile(location, zones);
+            case "rock":
+                return new RockTile(location, zones);
+            case "mountains":
+                return new MountainsTile(location, zones);
+            case "desert":
+                return new DesertTile(location, zones);
+            case "sea":
+                return new SeaTile(location, zones);
+        }
+
+
+        return null;
+    }
+
 
     //This is awful - don't look up to it
     private Tile parse(String s) {
