f0a79847f78175a91668bb0842e3ad896a142613
Or Oz <oroz77@gmail.com>
2017-03-28 09:32:15 -0400

    began implementation according to design doc

--- src/main/controller/MapEditorController.java
+++ src/main/controller/MapEditorController.java
@@ -1,9 +1,34 @@
 package controller;
 
+import view.MapEditorPanel;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description:
  * Responsibilities:
  */
 public class MapEditorController {
+    private KeyEventController keyEventController;
+
+    public MapEditorController(){
+
+    }
+
+    public void cycleTerrain(){
+
+    }
+
+    public void cycleRiverCount(){
+
+    }
+
+    public void cycleOrientation(){
+
+    }
+
+    public void setKeyEventController(KeyEventController keyEventController){
+        this.keyEventController = keyEventController;
+    }
+
+    //TODO add remaining methods from design doc
 }
