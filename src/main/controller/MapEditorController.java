23822df6089c7e6b0e37efca08b87fe42d39bd85
Or Oz <oroz77@gmail.com>
2017-03-28 09:32:15 -0400

    began implementation according to design doc

--- src/main/controller/MapEditorController.java
+++ src/main/controller/MapEditorController.java
@@ -1,20 +1,31 @@
 package controller;
 
+import view.MapEditorPanel;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description: Handles input received from KeyEventController & ViewModelAdapter
  * Responsibilities: Reacts to input by displaying the actions on the Map Editor Panel
  */
 public class MapEditorController {
-    KeyEventController keyEventController;
+    private KeyEventController keyEventController;
 
-    public void cycleTerrain(){}
-    public void cycleRiverCount(){}
-    public void cycleOrientation(){}
-    public void save(){}
-    public void exitMapEditor(){}
-    public void addSelectedTile(){}
-    public void removeSelectedTile(){}
+    public MapEditorController(){
+
+    }
+
+    public void cycleTerrain(){
+
+    }
+
+    public void cycleRiverCount(){
+
+    }
+
+    public void cycleOrientation(){
+
+    }
+
     public void setKeyEventController(KeyEventController keyEventController){
         this.keyEventController = keyEventController;
     }
