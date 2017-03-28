39c268d1179734470a99e006f189076c4f41480e
Or Oz <oroz77@gmail.com>
2017-03-28 09:32:41 -0400

    added reference to MapEditorController

--- src/main/controller/KeyEventController.java
+++ src/main/controller/KeyEventController.java
@@ -1,9 +1,18 @@
 package controller;
 
+import javax.swing.text.View;
+
 /**
  * Created by TheNotoriousOOP on 3/26/2017.
  * Class Description:
  * Responsibilities:
  */
 public class KeyEventController {
+    private MapEditorController mec;
+    private View view;
+
+    public KeyEventController(MapEditorController mec, View view){
+        this.mec = mec;
+        this.view = view;
+    }
 }
