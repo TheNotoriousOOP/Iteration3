56132a9d08ace339a4ca14e33270ff5365a80d81
Or Oz <oroz77@gmail.com>
2017-03-29 01:27:18 -0400

    parser works accurately

--- src/main/model/map/ParseMap.java
+++ src/main/model/map/ParseMap.java
@@ -4,6 +4,7 @@ import model.map.tile.*;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * Created by Customer-PC on 3/26/2017.
@@ -35,10 +36,8 @@ public class ParseMap {
     }
 
 
-    //written by Or, needs to be tested
     private Tile parseStringForTile(String s) {
 
-        s = s.replaceAll("\\s+","");    //remove all whitespaces
 
         boolean hasRiver = false;
         int numberOfParenthesesPair = s.length() - s.replaceAll("\\)", "").length();  //count pairs of parentheses
@@ -53,12 +52,12 @@ public class ParseMap {
         String locationString = s.substring(indexOfLocationLeftParenthesis + 1, indexOfLocationRightParenthesis);
 
 
-        int indexOfTerrainBegin = indexOfLocationRightParenthesis + 1;
+        int indexOfTerrainBegin = indexOfLocationRightParenthesis + 2;
         int indexOfTerrainEnd = s.length() - 1;
         if(hasRiver){   //if a river exists, overwrite the end of terrain index
             indexOfTerrainEnd = s.lastIndexOf('(') - 1;
         }
-        String terrain = s.substring(indexOfTerrainBegin, indexOfTerrainEnd + 1);
+        String terrain = s.substring(indexOfTerrainBegin, indexOfTerrainEnd);
 
 
         Zone[] zones = new Zone[6];
@@ -74,33 +73,28 @@ public class ParseMap {
 
             String river = s.substring(indexOfRiverStart + 1, indexOfRiverEnd);
 
-            //initializing rivered zones correctly
-            for (Character c : river.toCharArray()){
-                int zoneFace = Character.getNumericValue(c);
-                zones[zoneFace-1] = new Zone(true, false);
+            Scanner riverScanner = new Scanner(river);
+
+            while(riverScanner.hasNextInt()){
+                int zoneFace = riverScanner.nextInt();
+                zones[zoneFace-1] = new Zone(true,false);
             }
+
 
         }
 
 
         //array of strings to correspond to x,y,z coords of cubevector
         String[] xyz = new String[3];
-
-        int i = 0;  //iterator
-        int iTmp;   //used for substring
         int indx = 0;   //index in xyz array
-        while( i < locationString.length() ){   //iterate through the location string
-            iTmp = i;   //reset temp
 
-            if (locationString.charAt(i) == '-'){   //if a '-' exists, the coord is a negative value
-                xyz[indx] = locationString.substring(i, iTmp + 2);  //add to the xyz array the current char (-) & the number following
-                i++;
-            } else{
-                xyz[indx] = locationString.substring(i,iTmp + 1 );  //add to the xyz array the number
-            }
-            i++;
+        Scanner scanner = new Scanner(locationString);
+        while ((scanner.hasNext())){
+            xyz[indx] = scanner.next();
             indx++;
         }
+
+
 
         int x = Integer.valueOf(xyz[0]);
         int y = Integer.valueOf(xyz[1]);
@@ -108,7 +102,9 @@ public class ParseMap {
 
         CubeVector location = new CubeVector(x, y, z);
 
-
+        System.out.println("PARSER: loc " + location.toString());
+        System.out.println("PARSER: riv " + zones.toString());
+        System.out.println("PARSER: ter " + terrain);
         switch (terrain){
             case "woods":
                 return new WoodsTile(location, zones);
@@ -131,44 +127,5 @@ public class ParseMap {
         return null;
     }
 
-    //TODO remove?
-    //This is awful - don't look up to it
-    private Tile parse(String s) {
-        System.out.println("Parsing string " + s);
-        boolean river = s.length() > 6;
-        String[] parts = s.split("\\s+");
-        CubeVector v = new CubeVector(Integer.decode(parts[1]), Integer.decode(parts[2]), Integer.decode(parts[3]));
-        Zone[] zones = new Zone[6];
-        if(river) {
-            for(int iii = 0; iii <6; iii++)
-                zones[iii] = new Zone(false, true);
-            int connections = s.length() - 8;
-            boolean[] water = new boolean[6];
-            for(int iii = 0; iii < connections; iii++) {
-                water[Integer.decode(parts[7+iii])] = true;
-            }
-            for(int iii = 0; iii <6; iii++) {
-                zones[iii] = new Zone(water[iii], true);
-            }
 
-        } else {
-            for(int iii = 0; iii <6; iii++)
-                zones[iii] = new Zone(false, true);
-        }
-        switch(parts[5]) {
-            case "woods":
-                return new WoodsTile(v, zones);
-            case "pasture":
-                return new PastureTile(v, zones);
-            case "rock":
-                return new RockTile(v, zones);
-            case "mountains":
-                return new MountainsTile(v, zones);
-            case "desert":
-                return new DesertTile(v, zones);
-            case "sea":
-                return new SeaTile(v, zones);
-        }
-        return null;
-    }
 }
