5d4b8bffc001411b8ed174cea6f7536a5ba344c9
Or Oz <oroz77@gmail.com>
2017-04-14 18:57:03 -0400

    extends phase observer now

--- src/main/model/map/tile/nodeRepresentation/nodes/parent/ParentNode.java
+++ src/main/model/map/tile/nodeRepresentation/nodes/parent/ParentNode.java
@@ -1,9 +1,11 @@
 package model.map.tile.nodeRepresentation.nodes.parent;
 
+import model.ability_management.AbilityEnum;
 import model.map.tile.nodeRepresentation.NodeRepresentation;
 import model.map.tile.nodeRepresentation.nodes.Node;
 import model.map.tile.nodeRepresentation.nodes.child.ChildLandNode;
 import model.map.tile.nodeRepresentation.nodes.child.ChildNode;
+import model.phase.observers.PhaseObserver;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,9 +15,10 @@ import java.util.HashMap;
  * maps integer (of face), to children nodes corresponds to (1, 2, 3, 4, 5, 6)
  * children nodes hashmap corresponds to (-1, 0, 1)
  */
-public abstract class ParentNode extends Node {
+public abstract class ParentNode extends Node implements PhaseObserver{
     private HashMap<Integer, HashMap<Integer, ChildNode>> childrenNodes;
     private NodeRepresentation nodeRepresentation;
+    private ArrayList<AbilityEnum> abilitiesOnNode = new ArrayList<>();
 
     public ParentNode(NodeRepresentation nodeRepresentation){
         this.nodeRepresentation = nodeRepresentation;
@@ -35,5 +38,38 @@ public abstract class ParentNode extends Node {
 
     public HashMap<Integer, ChildNode> getChildNodesOnFace(int face){
         return childrenNodes.get(face);
+    }
+
+    @Override
+    public void onTradePhaseStart() {
+        //TODO implement
+    }
+
+    @Override
+    public void onProductionPhaseStart() {
+        //TODO implement
+    }
+
+    @Override
+    public void onBuildPhaseStart() {
+        //TODO implement
+    }
+
+    @Override
+    public void onMovementPhaseStart() {
+        //TODO fix
+        for(HashMap<Integer, ChildNode> childMapping : childrenNodes.values()){
+            for(ChildNode c : childMapping.values()){
+               //TODO add a check if that movement is allowed, because a childnode may not be completed
+                abilitiesOnNode.add(c.getDirection().getAbilityEnum());
+            }
+        }
+
+
+    }
+
+    @Override
+    public void onWonderPhaseStart() {
+        //TODO implement
     }
 }
