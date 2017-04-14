46c46166f63723d881be058d0bdb0d1d6faa8b74
Or Oz <oroz77@gmail.com>
2017-04-14 19:27:28 -0400

    fixed direction constructors

--- src/main/model/map/tile/nodeRepresentation/nodes/direction/NorthEast.java
+++ src/main/model/map/tile/nodeRepresentation/nodes/direction/NorthEast.java
@@ -1,5 +1,6 @@
 package model.map.tile.nodeRepresentation.nodes.direction;
 
+import model.ability_management.AbilityEnum;
 import model.ability_management.ability.move_abilities.MoveNorthEastAbility;
 
 /**
@@ -7,6 +8,6 @@ import model.ability_management.ability.move_abilities.MoveNorthEastAbility;
  */
 public class NorthEast extends ChildDirection {
     public NorthEast() {
-        setAbility(new MoveNorthEastAbility());
+        setAbilityEnum(AbilityEnum.MOVE_NORTHEAST);
     }
 }
