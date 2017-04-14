d2ed00fa714f8af89e2c53bcc15570a77204c5a2
Or Oz <oroz77@gmail.com>
2017-04-14 18:20:44 -0400

    template for ability enum

---
+++ src/main/model/ability_management/AbilityEnum.java
@@ -0,0 +1,18 @@
+package model.ability_management;
+
+/**
+ * A collection of all possible abilities
+ * Enumerated with a string for parsing and view purposes
+ */
+public enum AbilityEnum {
+    MOVE_NORTH          ("Move North"),
+    MOVE_NORTHLEFT      ("Move North Left"),
+    MOVE_NORTHRIGHT      ("Move North Right");
+
+
+    private AbilityEnum(String s) {
+
+    }
+
+
+}
