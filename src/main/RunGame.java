363e8fdd77f313773f8b1b915b535fdb0ed423ba
TylerBarkley <tylerb306@ufl.edu>
2017-04-15 18:53:28 -0400

    I don't know what I'm doing

--- src/main/RunGame.java
+++ src/main/RunGame.java
@@ -3,6 +3,10 @@ import controller.MainMenuController;
 import controller.MapEditorController;
 import model.EditorModel;
 import model.game.GameModel;
+import model.map.CubeVector;
+import model.resources.TransportStorage;
+import model.transporters.TransporterID;
+import model.transporters.land_transporters.Donkey;
 import view.PanelManager;
 
 import javax.swing.*;
@@ -25,7 +29,8 @@ public class RunGame {
         GameController gameController = new GameController(panelManager.getGameViewPanel(), gameModel);
 
         MainMenuController mainMenuController = new MainMenuController(panelManager.getMainMenuPanel(), mapEditorController, gameController);
-
+        //GLASS BOX TESTING YOLO
+        gameModel.getPlayers()[0].addTransporter(new Donkey(new TransporterID(), gameModel.getPlayers()[0], new TransportStorage(2),null, null, 3) ));
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
