0beca94b4c4f3e2673b1051426d295eabbef7eb3
thkim1234 <tedkim1234@gmail.com>
2017-04-15 11:54:18 -0400

    Add brick function sorta implemented

--- src/main/view/WonderViewPanel.java
+++ src/main/view/WonderViewPanel.java
@@ -6,6 +6,8 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class WonderViewPanel extends JPanel {
 
@@ -14,6 +16,12 @@ public class WonderViewPanel extends JPanel {
     private Brick[][] secondBrickRow = new Brick[5][6];
     private Brick[][] thirdBrickRow = new Brick[4][5];
     private Brick[][] bottomBrickRow = new Brick[3][4];
+    private Phase[] phaseRow = new Phase[4];
+    private Circle[] sequenceChart = new Circle[6];
+
+    private int prayCircleXOffSet = 514;
+    private int prayCircleYOffSet = 20;
+    private int prayCircleXGap = 65;
 
     private int topBrickXOffSet = 515;
     private int topBrickYOffSet = 100;
@@ -24,9 +32,21 @@ public class WonderViewPanel extends JPanel {
     private int bottomBrickXOffSet = 612;
     private int bottomBrickYOffSet = 438;
 
+
+    private int phaseRowXOffSet = 600;
+    private int phaseRowYOffSet = 550;
+
+    private int sequenceChartXOffSet = 436;
+    private int sequenceChartYOffSet = 630;
+    private int sequenceChartXGap = 105;
+
+    private Color brown = new Color(156, 93, 82);
+
+    private int brickColumn = 0;
+    private int brickRow = bottomBrickRow.length-1;
     public WonderViewPanel(){
         for(int i = 0; i < prayCircles.length; i++){
-            prayCircles[i] = new Circle(i);
+            prayCircles[i] = new Circle(i, prayCircleXGap, prayCircleXOffSet, prayCircleYOffSet);
         }
         for(int i = 0; i < topBrickRow.length; i++){
             for(int j = 0; j < topBrickRow[i].length; j++) {
@@ -48,6 +68,34 @@ public class WonderViewPanel extends JPanel {
                 bottomBrickRow[i][j] = new Brick(i, j, bottomBrickXOffSet, bottomBrickYOffSet);
             }
         }
+        for(int i = 0; i < phaseRow.length; i++){
+            phaseRow[i] = new Phase(i, phaseRowXOffSet, phaseRowYOffSet);
+        }
+        for(int i = 0; i < sequenceChart.length; i++){
+            sequenceChart[i] = new Circle(i, sequenceChartXGap, sequenceChartXOffSet, sequenceChartYOffSet);
+        }
+        this.setLayout(new GridBagLayout());
+        JPanel sidePanel = new JPanel(new GridLayout());
+        JButton addBrick = new JButton("Add brick");
+        sidePanel.add(addBrick);
+        addBrick.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bottomBrickRow[brickRow][brickColumn].setColor(Color.red);
+                brickColumn++;
+                repaint();
+                if(brickColumn == bottomBrickRow[brickRow].length) {
+                    brickRow--;
+                    brickColumn = 0;
+                }
+            }
+        });
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.LINE_START;
+        c.weightx = 1;
+        c.weighty = 1;
+        this.add(sidePanel, c);
+
     }
     @Override
     public void paintComponent(Graphics g){
@@ -76,39 +124,51 @@ public class WonderViewPanel extends JPanel {
                 drawBottomBrickRow(bottomBrickRow[i][j], g2);
             }
         }
+        for(int i = 0; i < phaseRow.length; i++){
+            drawPhaseRow(phaseRow[i], g2);
+        }
+        for(int i = 0; i < sequenceChart.length; i++){
+            drawCircles(sequenceChart[i], g2);
+        }
     }
     public void drawCircles(Circle circle, Graphics2D g2){
         g2.setColor(Color.blue);
         g2.drawOval(circle.getX(),circle.getY(), 60, 60);
     }
     public void drawTopBrickRow(Brick brick, Graphics2D g2){
-        g2.setColor(Color.darkGray);
-        g2.drawRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
+        g2.setColor(brick.getColor());
+        g2.fillRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
     }
     public void drawSecondBrickRow(Brick brick, Graphics2D g2){
-        g2.setColor(Color.darkGray);
-        g2.drawRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
+        g2.setColor(brick.getColor());
+        g2.fillRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
     }
     public void drawThirdBrickRow(Brick brick, Graphics2D g2){
-        g2.setColor(Color.darkGray);
-        g2.drawRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
+        g2.setColor(brick.getColor());
+        g2.fillRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
     }
     public void drawBottomBrickRow(Brick brick, Graphics2D g2){
-        g2.setColor(Color.darkGray);
-        g2.drawRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
+        g2.setColor(brick.getColor());
+        g2.fillRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
     }
+    public void drawPhaseRow(Phase phase, Graphics2D g2){
+        g2.setColor(Color.darkGray);
+        g2.drawRect(phase.getX(), phase.getY(), phase.getWidth(), phase.getHeight());
+    }
+//    public void drawPlayerBrick(Brick brick, Graphics2D g2){
+//        g2.setColor(brick.getColor());
+//        g2.fillRect(brick.getX(), brick.getY(), brick.getWidth(), brick.getHeight());
+//    }
 }
 
 class Circle{
 
     private int x;
     private int y;
-    private int OFFSETX = 514;
-    private int OFFSETY = 20;
 
-    public Circle(int x){
-        this.x = x * 65 + OFFSETX;
-        this.y = x + OFFSETY;
+    public Circle(int i, int xGap, int circleXOffSet, int circleYOffSet){
+        this.x = i * xGap + circleXOffSet;
+        this.y = i + circleYOffSet;
     }
 
     public int getX(){
@@ -124,11 +184,44 @@ class Brick{
     private int y;
     private int height = 25;
     private int width = 60;
+    private Color defaultBrown = new Color(156, 93, 82);
+    private Color brickColor;
+    public Brick(int i, int j, int brickXOffSet, int brickYOffSet){
+        this.x = j * 65 + brickXOffSet;
+        this.y = i * 28 + brickYOffSet;
+        this.brickColor = defaultBrown;
+    }
 
-    public Brick(int i, int j, int topBrickXOffSet, int topBrickYOffSet){
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
+    public int getWidth(){
+        return this.width;
+    }
+    public int getHeight(){
+        return this.height;
+    }
+    public Color getColor(){
+        return this.brickColor;
+    }
+    public void setColor(Color color){
+        this.brickColor = color;
+    }
+}
 
-        this.x = j * 65 + topBrickXOffSet;
-        this.y = i * 28 + topBrickYOffSet;
+class Phase{
+    private int x;
+    private int y;
+    private int height = 45;
+    private int width = 60;
+
+    public Phase(int i, int phaseXOffSet, int phaseYOffSet){
+
+        this.x = i * 65 + phaseXOffSet;
+        this.y = phaseYOffSet;
     }
 
     public int getX(){
@@ -144,5 +237,7 @@ class Brick{
         return this.height;
     }
 }
+
+
 
 
