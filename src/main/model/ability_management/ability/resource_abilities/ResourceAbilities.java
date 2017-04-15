8ce9662fb23c4457db073d4c083a25d0a03c4490
TylerBarkley <tylerb306@ufl.edu>
2017-04-15 12:24:44 -0400

    Added resource abilities

---
+++ src/main/model/ability_management/ability/resource_abilities/ResourceAbilities.java
@@ -0,0 +1,31 @@
+package model.ability_management.ability.resource_abilities;
+
+/**
+ * Created by TheNotoriousOOP on 4/15/2017.
+ * Class Description:
+ * Responsibilities:
+ */
+public interface ResourceAbilities {
+
+    void dropOffBoard();
+    void dropOffClay();
+    void dropOffCoin();
+    void dropOffFuel();
+    void dropOffGold();
+    void dropOffGoose();
+    void dropOffIron();
+    void dropOffStock();
+    void dropOffTrunk();
+    void dropOffStone();
+    void pickUpBoard();
+    void pickUpClay();
+    void pickUpCoin();
+    void pickUpFuel();
+    void pickUpGold();
+    void pickUpGoose();
+    void pickUpIron();
+    void pickUpStock();
+    void pickUpTrunk();
+    void pickUpStone();
+
+}
