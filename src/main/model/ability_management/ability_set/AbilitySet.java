dd3016e6195aa0ee6a614a6e0191b057c1b9d91d
Jonathen Settle <jonathenksettle@ufl.edu>
2017-04-15 15:59:02 -0400

    Refactoring separate Iterators into a templated MyBidrectionalIterator.

--- src/main/model/ability_management/ability_set/AbilitySet.java
+++ src/main/model/ability_management/ability_set/AbilitySet.java
@@ -1,15 +1,17 @@
 package model.ability_management.ability_set;
 
 import model.ability_management.ability.Ability;
+import model.transporters.MyBidirectionalIterator;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 /**
  * Created by TheNotoriousOOP on 4/12/2017.
  * Class Description:
  * Responsibilities:
  */
-public class AbilitySet {
+public class AbilitySet implements Iterable<Ability> {
 
     private ArrayList<Ability> invalidAbilities;
     private ArrayList<Ability> validAbilities;
@@ -37,5 +39,10 @@ public class AbilitySet {
 
     public void setValidAbilities(ArrayList<Ability> validAbilities) {
         this.validAbilities = validAbilities;
+    }
+
+    @Override
+    public MyBidirectionalIterator<Ability> iterator() {
+        return new MyBidirectionalIterator<>(validAbilities);
     }
 }
